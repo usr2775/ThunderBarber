bc0cd1450d753b54b0d9aaa6487ebc869db5e765
Chibuike <usr2775@salesianos-lacuesta.net>
2024-05-24 20:04:54 +0100

    fix: que calendario sea privado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,40 +8,29 @@ import Signup from "./pages/Signup";
 import Navbar from './components/Navbar';
 import HairCare from "./pages/HairCare";
 import HairStyle from "./pages/HairStyles";
-import CalendarPage from "./pages/CalendarPage"
-
-
+import CalendarPage from "./pages/CalendarPage";
 
 function App() {
-  // Access user data from the authentication context
-
-  const { user } = useAuthContext()
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className=" p-5 m-auto">
-          <Routes>
-            <Route
-              path="/"
-              element={user ? <Home /> : <Navigate to="/login" />}
-            />
-            <Route
-              path="/login"
-              element={!user ? <Login /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/signup"
-              element={!user ? <Signup /> : <Navigate to="/" />}
-            />
-            <Route path="/CalendarPage" element={<CalendarPage />} />
-            <Route path="/haircare" element={<HairCare />} />
-            <Route path="/hairstyles" element={<HairStyle />} />
-          </Routes>
+    // Access user data from the authentication context
+    const { user } = useAuthContext();
+    
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <div className="p-5 m-auto">
+                    <Routes>
+                        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+                        <Route path="/CalendarPage" element={user ? <CalendarPage /> : <Navigate to="/login" />} />
+                        <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+                        <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
+                        <Route path="/haircare" element={<HairCare />} />
+                        <Route path="/hairstyles" element={<HairStyle />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
-      </BrowserRouter>
-    </div>
-  );
+    );
 }
 
 export default App;
